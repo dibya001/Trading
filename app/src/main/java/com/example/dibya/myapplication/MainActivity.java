6596f9593135b94844992b013025c481d7734999
dibya ranjan mishra <dibyamishra016@gmail.com>
2018-10-06 10:29:18 +0530

    alert confirmation

--- app/src/main/java/com/example/dibya/myapplication/MainActivity.java
+++ app/src/main/java/com/example/dibya/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.dibya.myapplication;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -8,6 +9,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -122,9 +124,27 @@ public class MainActivity extends AppCompatActivity {
             startActivity(i);
             // Toast.makeText(this,"hthtt",Toast.LENGTH_LONG).show();
         } else if (item.getItemId() == R.id.logout) {
-            signOut();
+            showAlert();
         }
         return true;
+    }
+
+    private void showAlert() {
+        AlertDialog.Builder ab = new AlertDialog.Builder(this);
+        ab.setMessage("Are You Sure to Log out?");
+        ab.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                signOut();
+            }
+        });
+        ab.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        ab.show();
     }
 
     private void signOut() {
